ee4aa10d5ada3464259ee22cffa9dd9206fc8e33
hellorafiul <hellorafiul@gmail.com>
2021-11-13 22:06:20 +0600

    initially done

--- index.js
+++ index.js
@@ -20,18 +20,59 @@ async function run() {
     const database = client.db('amentsCarShop');
     const orderCollection = database.collection('orders');
     const reviewCollection = database.collection('reviews');
+    const productCollection = database.collection('products');
 
-    // Post reviews
+    // Post reviews/Testimonial
     app.post('/reviews', async (req, res) => {
       const reviews = req.body;
       const result = await reviewCollection.insertOne(reviews);
       res.json(result)
     });
-    // get reviews
+    // get All reviews/Testimonial
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find({}).toArray();
       res.json(result)
     });
+
+    // Post/Add Product
+    app.post('/addProduct', async (req, res) => {
+      const reviews = req.body;
+      const result = await productCollection.insertOne(reviews);
+      res.json(result)
+    });
+
+    // get All Product
+    app.get('/products', async (req, res) => {
+      const { limit } = req.query
+
+      if (limit > 0) {
+        const result = await productCollection.find({}).limit(parseInt(limit)).toArray();
+        res.json(result)
+      } else {
+        const result = await productCollection.find({}).toArray();
+        res.json(result)
+      }
+    });
+
+
+    // Order Now
+    app.post("/orders", async (req, res) => {
+      console.log(req.body)
+      const result = await orderCollection.insertOne(req.body);
+      res.send(result)
+    })
+
+    // Load my Order 
+    app.get('/orders/:email', async (req, res) => {
+      const result = await orderCollection.find({ email: req.params.email }).toArray()
+      res.send(result)
+    })
+
+    // Load All Orders 
+    app.get('/orders', async (req, res) => {
+      const result = await orderCollection.find({}).toArray()
+      res.send(result)
+    })
 
   } finally {
 
